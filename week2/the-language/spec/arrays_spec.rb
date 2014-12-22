8da0fe10c0bbe4150f8c2906b8cae3ea83985171
Asta Bevainyte <asta.bevainyte@onthebeach.co.uk>
2014-12-22 14:11:44 +0000

    week2

---
+++ week2/the-language/spec/arrays_spec.rb
@@ -0,0 +1,133 @@
+RSpec.describe "Ruby Arrays" do
+  it "can be created like any other object" do
+    empty = Array.new
+
+
+    expect( empty.class ).to eq Array
+    expect( empty.size ).to eq 0
+
+  end
+
+  it "can also be created using an array literal" do
+    array = Array.new
+
+    array[0] = 1
+    expect( array ).to eq( [1] )
+
+    array[1] = 2
+    expect( array ).to eq( [1, 2] )
+
+    array << 333
+
+    expect( array ).to eq([1, 2, 333])
+
+  end
+
+  it "provides the subscript operator for accessing elements" do
+    array = ["New York", "Paris", "London", "Milan"]
+
+    expect( array[0] ).to eq( "New York" )
+    expect( array[3] ).to eq( "Milan" )
+
+    # What do you thing will happen here?
+    expect( array[-1] ).to eq("Milan")
+    expect( array[-3] ).to eq("Paris")
+
+  end
+
+  it "has methods for accessing the beginning and end of the array" do
+    array = ["New York", "Paris", "London", "Milan"]
+
+    expect( array.first ).to eq( "New York" )
+    expect( array.last  ).to eq( "Milan" )
+  end
+
+  it "can be sliced" do
+    array = ["New York", "Paris", "London", "Milan"]
+
+
+    expect( array.slice(0,1) ).to eq( ["New York"] )
+    expect( array.slice(0,2) ).to eq( ["New York", "Paris"] )
+    expect( array.slice(3,3) ).to eq( ["Milan"] )
+    expect( array.slice(2,20) ).to eq( ["London", "Milan"] )
+
+    expect( array.slice(4,0) ).to eq( [] )
+    expect( array.slice(4,10) ).to eq( [] )
+    expect( array.slice(5,0) ).to eq( nil )
+  end
+
+  it "is similar to a range" do
+    expect( (1..5).class ).to eq( Range )
+    expect( [1,2,3,4,5] ).not_to eq( (1..5) )
+
+    expect( [1, 2, 3, 4, 5] ).to eq( (1..5).to_a )
+    expect( [1, 2, 3 ,4] ).to eq( (1...5).to_a )
+
+  end
+
+  it "can be sliced by a range" do
+    array = ["New York", "Paris", "London", "Milan"]
+
+    expect( ["New York", "Paris", "London"] ).to eq array[0..2]
+    expect( ["New York", "Paris"] ).to eq array[0...2]
+    expect( ["London", "Milan"] ).to eq array[2..-1]
+    expect( [] ).to eq array[-2..1]
+  end
+
+  it "can be used as a stack" do
+    array = [1,2]
+
+    array.push("on the end")
+
+    expect( array ).to eq( [1, 2, "on the end"] )
+
+    value = array.pop
+    expect( value ).to eq( "on the end" )
+    expect( array ).to eq( [1, 2] )
+  end
+
+  it "can be used as a queue" do
+    array = [:first, :second]
+    array.push(:third)
+
+    value = array.shift
+    expect( value ).to eq( :first )
+    expect( array ).to eq( [:second, :third] )
+
+    # Sometimes you need to get things to the front of a queue
+    array.unshift(value)
+
+    expect( array ).to eq( [:first, :second, :third] )
+
+  end
+
+  context 'product' do
+    it 'returns an array of all combinations of elements from all arrays' do
+      expect([1, 2].product([3])).to eq [[1, 3], [2, 3]]
+
+      product = [1, 2].product([3], [4, 5])
+
+      expect(product).to eq [[1, 3, 4], [1, 3, 5], [2, 3, 4], [2, 3, 5]]
+    end
+
+  end
+
+  it "Finding the first x" do
+    array = ["item 1", 2, "item 3", "rabbits"]
+    expect( array.first ).to eq( "item 1" )
+    expect( array.first(2) ).to eq( ["item 1", 2] )
+  end
+
+  it "Returning the last x elements " do
+    array = [1,2,3,4,5]
+    expect(array.last).to eq(5)
+    expect(array.last(2)).to eq([4,5])
+  end
+
+  it "Dropping the last x elements " do
+    array = [1,2,3,4,5]
+    expect(array.drop(3)).to eq([4 ,5])
+    expect(array.take(3)).to eq([1 ,2, 3])
+  end  
+
+end
